be79acbbff57da5727d6ecce22e288e992c13177
Krzysztof Kubiś <kkubis44386@gmail.com>
2022-12-02 09:10:24 +0100

    init
    -created git repository
    -created basic project from Spring Initializr

---
+++ src/main/java/com/app/geolocalized/GeoLocalizedApplication.java
@@ -0,0 +1,13 @@
+package com.app.geolocalized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoLocalizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoLocalizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/geolocalized/GeoLocalizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.geolocalized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoLocalizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
